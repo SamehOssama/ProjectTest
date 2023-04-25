1042a5bef70f8089ce8ec923a5bfff01edfc4582
a-abdou-abdallah <130511107+a-abdou-abdallah@users.noreply.github.com>
2023-04-25 18:38:27 +0200

    Add files via upload

---
+++ timer_function.c
@@ -0,0 +1,18 @@
+void timer_1ms(void) 
+{
+	NVIC_ST_CTRL_R=0X0;    
+	NVIC_ST_CURRENT_R=0;
+	NVIC_ST_RELOAD_R= 80000-1;
+	NVIC_ST_CTRL_R=0X5;
+	WHILE((NVIC_ST_CTRL_R & 0x00010000) ==0);
+
+}
+
+void timer_multiples_1ms(uint32_t delay) //this function creates "delay" ms
+{ 
+	uint32_t time;
+    for(int i=0;i<time;i++)
+	{
+		timer_1ms();
+    }
+}
