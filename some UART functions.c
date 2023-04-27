6ec014528b5815a8d4e5922f03a194dee12218ba
Omar Gamal <112751175+ElecSpartan@users.noreply.github.com>
2023-04-27 02:54:00 +0200

    Update some UART functions.c

--- some UART functions.c
+++ some UART functions.c
@@ -1,4 +1,8 @@
-		void UART_Init(void){
+		
+//values need to be fixed to satisfy UART1
+
+   
+   void UART1_Init(void){
 			SYSCTL_RCGCUART_R |=OX01;
 			SYSCTL_RCGCGPIO_R |=OX01;
 			UART0_CTL_R &=~0X01;
@@ -13,26 +17,21 @@
 		}
 		
 		
-		bool UART0_Available(void){
+		bool UART1_Available(void){
 			return (UART0_FR_R & 0X10 !=0) ? 0:1;
 		}
-		char UART0_Read(void){
+
+
+		char UART1_Read(void){
 			while(UART0_FR_R & OX10 !=0);
 			return (char)(UART0_DR_R & 0XFF);
 		}
-		void UART0_Write(char data){
+
+
+		void UART1_Write(char data){
 			while(UART0_FR_R & OX20 !=0);
 			UART0_DR_R=data;
 		}
-		void read_WriteToUpper(void){
-			char in;
-			char out;
-			while(1){
-				in=UART0_Read();
-				out=in-0x20;
-				UART0_Write(out);
-			}
-		}
-			
+	
 			
 			
