21f88388a22119555f598453af2330154b4c905b
Omar Gamal <112751175+ElecSpartan@users.noreply.github.com>
2023-05-04 14:55:52 +0300

    Create calculateDistance.c
    calculates distance in Km

---
+++ calculateDistance.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+#define RADIUS 6371 // radius of earth in kilometers
+
+double to_radians(double degrees) {
+    return degrees * M_PI / 180.0;
+}
+
+double distance(double lat1, double lon1, double lat2, double lon2) {
+    double dlat = to_radians(lat2 - lat1);
+    double dlon = to_radians(lon2 - lon1);
+    double a = pow(sin(dlat / 2), 2) + cos(to_radians(lat1)) * cos(to_radians(lat2)) * pow(sin(dlon / 2), 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return RADIUS * c;
+}
