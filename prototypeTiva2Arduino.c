058efeeae6884fedfd5bdb7eeba0adcd765658c0
Waseem Faheem <88598649+GyroZeppeliLovesBalls@users.noreply.github.com>
2023-05-08 02:31:08 +0300

    Update prototypeTiva2Arduino.c
    Added Driver Functions for a 2×16 LCD in its 8-bit Mode

--- prototypeTiva2Arduino.c
+++ prototypeTiva2Arduino.c
@@ -18,77 +18,6 @@ void SystemInit(){}
 #define GREEN 0x08
 #define SW1   0x10
 
-void digitalPinMode(char port, uint32_t pin, uint32_t mode){
-    uint32_t pinD = 1 << pin, clk = 1;
-    volatile uint32_t* baseAddress;
-
-    switch(port){
-        case 'A': baseAddress = GPIO_PORTA_DATA_BITS_R; clk = clk << 0; if(pin>7) return; break;
-        case 'B': baseAddress = GPIO_PORTB_DATA_BITS_R; clk = clk << 1; if(pin>7) return; break;
-        case 'C': baseAddress = GPIO_PORTC_DATA_BITS_R; clk = clk << 2; if(pin>7) return; break;
-        case 'D': baseAddress = GPIO_PORTD_DATA_BITS_R; clk = clk << 3; if(pin>7) return; break;
-        case 'E': baseAddress = GPIO_PORTE_DATA_BITS_R; clk = clk << 4; if(pin>5) return; break;
-        case 'F': baseAddress = GPIO_PORTF_DATA_BITS_R; clk = clk << 5; if(pin>4) return; break;
-        default: return;
-    }
-
-    SYSCTL_RCGCGPIO_R |= clk; // Connect Clock to Port
-    while((SYSCTL_PRGPIO_R & clk) == 0); // Wait for Clock
-
-    *((volatile uint32_t*)(baseAddress + (0x520/sizeof(uint32_t))))  = 0x4C4F434B; // Unlock
-
-    *((volatile uint32_t*)(baseAddress + (0x524/sizeof(uint32_t)))) |=  pinD; // CR
-
-    *((volatile uint32_t*)(baseAddress + (0x420/sizeof(uint32_t)))) &= ~pinD; // AFSEL
-    *((volatile uint32_t*)(baseAddress + (0x52C/sizeof(uint32_t)))) &= ~((0xf)<<(pin*4)); // PCTL
-    *((volatile uint32_t*)(baseAddress + (0x528/sizeof(uint32_t)))) &= ~pinD; // AMSEL
-    *((volatile uint32_t*)(baseAddress + (0x51C/sizeof(uint32_t)))) |=  pinD; // DEN
-
-    if(mode == 1){ // Output
-        *((volatile uint32_t*)(baseAddress + (0x400/sizeof(uint32_t)))) |=  pinD; // DIR
-        *((volatile uint32_t*)(baseAddress + (0x510/sizeof(uint32_t)))) |=  pinD; // PUR
-        *((volatile uint32_t*)(baseAddress + (0x514/sizeof(uint32_t)))) &= ~pinD; // PDN
-    }
-    else if(mode == 0){ // Input
-        *((volatile uint32_t*)(baseAddress + (0x400/sizeof(uint32_t)))) &= ~pinD; // DIR
-        *((volatile uint32_t*)(baseAddress + (0x510/sizeof(uint32_t)))) &= ~pinD; // PUR
-        *((volatile uint32_t*)(baseAddress + (0x514/sizeof(uint32_t)))) |=  pinD; // PDN
-    }
-
-    *((volatile uint32_t*)baseAddress) &= ~pinD; // DATA
-}
-
-void portFInit(){
-
-    SYSCTL_RCGCGPIO_R |= 0x20;
-    while((SYSCTL_PRGPIO_R & 0x20) == 0) {}
-
-    GPIO_PORTF_AFSEL_R &= ~0x1f;
-    GPIO_PORTF_PCTL_R &= ~0x1f;
-    GPIO_PORTF_AMSEL_R &= ~0x1f;
-    GPIO_PORTF_DIR_R |= 0xE;
-    GPIO_PORTF_DEN_R |= 0x1f;
-    GPIO_PORTF_PUR_R &= ~0xE;
-    GPIO_PORTF_DATA_R &= ~0x1f;
-
-}
-
-void LED_ON(uint8_t LED){
-    GPIO_PORTF_DATA_R |= LED;
-}
-
-void LED_OFF(uint8_t LED){
-    GPIO_PORTF_DATA_R &= ~LED;
-}
-
-uint8_t switch1(){
-    return ((GPIO_PORTF_DATA_R & SW1) >> 4);
-}
-
-uint8_t switch2(){
-    return (GPIO_PORTF_DATA_R & SW2);
-}
-
 void SYSTICK_TIMER_Init(void){
     NVIC_ST_CTRL_R = 0;
     NVIC_ST_RELOAD_R = 0x0;
@@ -159,6 +88,244 @@ void delayMicros(uint32_t delay){
 void delays(uint32_t delay){
     while(delay-->0)
         wait1s();
+}
+
+void digitalPinMode(char port, uint32_t pin, uint32_t mode){
+    uint32_t pinD = 1 << pin, clk = 1;
+    volatile uint32_t* baseAddress;
+
+    switch(port){
+        case 'A': baseAddress = GPIO_PORTA_DATA_BITS_R; clk = clk << 0; if(pin>7) return; break;
+        case 'B': baseAddress = GPIO_PORTB_DATA_BITS_R; clk = clk << 1; if(pin>7) return; break;
+        case 'C': baseAddress = GPIO_PORTC_DATA_BITS_R; clk = clk << 2; if(pin>7) return; break;
+        case 'D': baseAddress = GPIO_PORTD_DATA_BITS_R; clk = clk << 3; if(pin>7) return; break;
+        case 'E': baseAddress = GPIO_PORTE_DATA_BITS_R; clk = clk << 4; if(pin>5) return; break;
+        case 'F': baseAddress = GPIO_PORTF_DATA_BITS_R; clk = clk << 5; if(pin>4) return; break;
+        default: return;
+    }
+
+    SYSCTL_RCGCGPIO_R |= clk; // Connect Clock to Port
+    while((SYSCTL_PRGPIO_R & clk) == 0); // Wait for Clock
+
+    *((volatile uint32_t*)(baseAddress + (0x520/sizeof(uint32_t))))  = 0x4C4F434B; // Unlock
+
+    *((volatile uint32_t*)(baseAddress + (0x524/sizeof(uint32_t)))) |=  pinD; // CR
+
+    *((volatile uint32_t*)(baseAddress + (0x420/sizeof(uint32_t)))) &= ~pinD; // AFSEL
+    *((volatile uint32_t*)(baseAddress + (0x52C/sizeof(uint32_t)))) &= ~((0xf)<<(pin*4)); // PCTL
+    *((volatile uint32_t*)(baseAddress + (0x528/sizeof(uint32_t)))) &= ~pinD; // AMSEL
+    *((volatile uint32_t*)(baseAddress + (0x51C/sizeof(uint32_t)))) |=  pinD; // DEN
+
+    if(mode == 1){ // Output
+        *((volatile uint32_t*)(baseAddress + (0x400/sizeof(uint32_t)))) |=  pinD; // DIR
+        *((volatile uint32_t*)(baseAddress + (0x510/sizeof(uint32_t)))) |=  pinD; // PUR
+        *((volatile uint32_t*)(baseAddress + (0x514/sizeof(uint32_t)))) &= ~pinD; // PDN
+    }
+    else if(mode == 0){ // Input
+        *((volatile uint32_t*)(baseAddress + (0x400/sizeof(uint32_t)))) &= ~pinD; // DIR
+        *((volatile uint32_t*)(baseAddress + (0x510/sizeof(uint32_t)))) &= ~pinD; // PUR
+        *((volatile uint32_t*)(baseAddress + (0x514/sizeof(uint32_t)))) |=  pinD; // PDN
+    }
+
+    *((volatile uint32_t*)baseAddress) &= 1; // DATA
+}
+
+void digitalPortMode(char port, uint32_t mode){
+    int i = (port == 'F')? 5 : (port == 'E')? 6 : 8, j = 0;
+    while(j<i){
+        digitalPinMode(port, j++, mode%2);
+        mode = mode >> 1;
+    }
+}
+
+void digitalPinWrite(char port, uint32_t pin, uint32_t out){
+    uint32_t pinD = 1 << pin, pinOffset;
+    volatile uint32_t* baseAddress;
+
+    switch(port){
+        case 'A': baseAddress = GPIO_PORTA_DATA_BITS_R; if(pin>7) return; break;
+        case 'B': baseAddress = GPIO_PORTB_DATA_BITS_R; if(pin>7) return; break;
+        case 'C': baseAddress = GPIO_PORTC_DATA_BITS_R; if(pin>7) return; break;
+        case 'D': baseAddress = GPIO_PORTD_DATA_BITS_R; if(pin>7) return; break;
+        case 'E': baseAddress = GPIO_PORTE_DATA_BITS_R; if(pin>5) return; break;
+        case 'F': baseAddress = GPIO_PORTF_DATA_BITS_R; if(pin>4) return; break;
+        default: return;
+    }
+
+    if(!((*((volatile uint32_t*)(baseAddress + (0x400/sizeof(uint32_t))))) | pinD))
+        return;
+
+    switch (pin) {
+        case 0:
+            pinOffset = 0x04;
+            break;
+        case 1:
+            pinOffset = 0x08;
+            break;
+        case 2:
+            pinOffset = 0x10;
+            break;
+        case 3:
+            pinOffset = 0x20;
+            break;
+        case 4:
+            pinOffset = 0x40;
+            break;
+        case 5:
+            pinOffset = 0x80;
+            break;
+        case 6:
+            pinOffset = 0x100;
+            break;
+        case 7:
+            pinOffset = 0x200;
+            break;
+        default:
+            return;
+    }
+
+    if(out == 0)
+        *(((volatile uint32_t*)baseAddress + (pinOffset/sizeof(uint32_t)))) &= ~pinD;
+    else if(out == 1)
+        *(((volatile uint32_t*)baseAddress + (pinOffset/sizeof(uint32_t)))) |=  pinD;
+    else
+        return;
+}
+
+void digitalPortWrite(char port, uint32_t data){
+    int i = (port == 'F')? 5 : (port == 'E')? 6 : 8, j = 0;
+    while(j<i){
+        digitalPinWrite(port, j++, data%2);
+        data = data >> 1;
+    }
+}
+
+#define LCD_Write 0
+#define LCD_Read 1
+#define LCD_Command 0
+#define LCD_Data 1
+
+void LCD_MOSI8bitEn(){
+    digitalPinMode('D', 0, 1);
+    digitalPinMode('D', 1, 1);
+    digitalPinMode('D', 2, 1);
+    digitalPinMode('D', 3, 1);
+    digitalPinMode('E', 0, 1);
+    digitalPinMode('E', 1, 1);
+    digitalPinMode('E', 2, 1);
+    digitalPinMode('E', 3, 1);
+
+    /*
+        A7 A6 A5    E3 E2 E1 E0 D3 D2 D1 D0
+        || || ||    || || || || || || || ||
+        RS RW EN    D7 D6 D5 D4 D3 D2 D1 D0
+    */
+
+    digitalPinMode('A', 5, 1);
+    digitalPinMode('A', 6, 1);
+    digitalPinMode('A', 7, 1);
+
+}
+
+void LCD_MOSI8bit(uint32_t mosi){
+
+    /*
+        E3 -> A0 -> D7
+        E2 -> A1 -> D6
+        E1 -> A2 -> D5
+        E0 -> A3 -> D4
+        D3 -> A4 -> D3
+        D2 -> A5 -> D2
+        D1 -> A6 -> D1
+        D0 -> A7 -> D0
+
+        E3 E2 E1 E0 D3 D2 D1 D0
+        || || || || || || || ||
+        D7 D6 D5 D4 D3 D2 D1 D0
+    */
+
+    //digitalPortWrite('D', mosi & 0x0f);
+    //digitalPortWrite('E', (mosi >> 4) & 0x0f);
+
+    *(GPIO_PORTD_DATA_BITS_R + (0x03C/sizeof(uint32_t))) = (mosi & 0x0f);
+    *(GPIO_PORTE_DATA_BITS_R + (0x03C/sizeof(uint32_t))) = ((mosi >> 4) & 0x0f);
+}
+
+void LCD_MOSI8bitCommand(uint32_t command){
+
+    digitalPinWrite('A', 5, 0);
+    LCD_MOSI8bit(command);
+    digitalPinWrite('A', 7, LCD_Command);
+    digitalPinWrite('A', 6, LCD_Write);
+
+    digitalPinWrite('A', 5, 1);
+    delayMillis(1);
+    digitalPinWrite('A', 5, 0);
+	
+    LCD_MOSI8bit(0);
+}
+
+void LCD_MOSI8bitChar(uint32_t data){
+
+    digitalPinWrite('A', 5, 0);
+    LCD_MOSI8bit(data);
+    digitalPinWrite('A', 7, LCD_Data);
+    digitalPinWrite('A', 6, LCD_Write);
+
+    digitalPinWrite('A', 5, 1);
+    delayMillis(1);
+    digitalPinWrite('A', 5, 0);
+
+    LCD_MOSI8bit(0);
+
+    LCD_MOSI8bitCommand(0x06);
+}
+
+void LCD_MOSI8bitStr(unsigned char* pt){
+    while(*pt){
+        LCD_MOSI8bitChar(*pt);
+        pt++;
+    }
+}
+
+void LCD_8bit_Init(){
+    LCD_MOSI8bitEn();
+    LCD_MOSI8bitCommand(0x30);
+    LCD_MOSI8bitCommand(0x0f);
+    LCD_MOSI8bitCommand(0x01);
+    LCD_MOSI8bitCommand(0x02);
+    LCD_MOSI8bitCommand(0x38);
+}
+
+void portFInit(){
+
+    SYSCTL_RCGCGPIO_R |= 0x20;
+    while((SYSCTL_PRGPIO_R & 0x20) == 0) {}
+
+    GPIO_PORTF_AFSEL_R &= ~0x1f;
+    GPIO_PORTF_PCTL_R &= ~0x1f;
+    GPIO_PORTF_AMSEL_R &= ~0x1f;
+    GPIO_PORTF_DIR_R |= 0xE;
+    GPIO_PORTF_DEN_R |= 0x1f;
+    GPIO_PORTF_PUR_R &= ~0xE;
+    GPIO_PORTF_DATA_R &= ~0x1f;
+
+}
+
+void LED_ON(uint8_t LED){
+    GPIO_PORTF_DATA_R |= LED;
+}
+
+void LED_OFF(uint8_t LED){
+    GPIO_PORTF_DATA_R &= ~LED;
+}
+
+uint8_t switch1(){
+    return ((GPIO_PORTF_DATA_R & SW1) >> 4);
+}
+
+uint8_t switch2(){
+    return (GPIO_PORTF_DATA_R & SW2);
 }
 
 void UART_Init(char port, uint32_t UART, char mode, uint32_t baudRate){
@@ -314,7 +481,11 @@ void UART_serialOutput(uint32_t UART, unsigned char* pt){
 
 void setup(){
     SystemInit();
+    SYSTICK_TIMER_Init();
+    LCD_8bit_Init();
 
+    LCD_MOSI8bitCommand(0x58);
+    
 }
 
 void loop(){
@@ -322,7 +493,7 @@ void loop(){
 }
 
 int main(){
-	
+
 	setup();
 	
 	while(1){
